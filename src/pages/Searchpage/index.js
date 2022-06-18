6c72c2ab0a9e173af90684ea6f32edc494e49c38
hishamkhattab <khattab648@gmail.com>
2022-06-19 01:56:41 +0200

    create search feature with error message handling

--- src/pages/Searchpage/index.js
+++ src/pages/Searchpage/index.js
@@ -1,9 +1,35 @@
+import { useState,useEffect,useRef } from "react";
+import {useSelector, useDispatch} from "react-redux";
 import { Link } from "react-router-dom";
+
+//components
+import BookShelf from "../../components/BookShelf";
+
+//reducers
+import { searchBook } from "../../store/bookSlice";
 
 //style
 import "./style.css";
 
 const Searchpage = () => {
+    const { isLoading, searchBookList } = useSelector(state => state.books);
+    const dispatch = useDispatch();
+    const searchRef = useRef(null);
+
+    const [searchParam, setSearchParam] = useState('');
+
+    useEffect(() => {
+        searchRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        if (searchParam !== "") {
+            dispatch(searchBook({query:searchParam,maxResult:10}));
+        } else {
+            dispatch(searchBook({query:'',maxResult:10}));
+        }
+    }, [dispatch, searchParam]);
+    
     return (
         <div className="search-books">
             <div className="search-books-bar">
@@ -14,14 +40,23 @@ const Searchpage = () => {
                     Close
                 </Link>
                 <div className="search-books-input-wrapper">
-                    <input
+                <input
                         type="text"
+                        ref={searchRef}
+                        value={searchParam}
+                        onChange={(e) => setSearchParam(e.target.value)}
                         placeholder="Search by title, author, or ISBN"
                     />
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+            {isLoading && <p>Loading ....</p>}
+                {!isLoading && searchBookList.length > 0 &&
+                    <BookShelf bookList={searchBookList.filter(book => book.imageLinks)} />
+                }
+                {!isLoading && Object.keys(searchBookList).includes("error") &&
+                    <p className="error-msg">There is no matched books</p>
+                }
             </div>
         </div>
     );
