5fed934621490f3166b4c684eafda9f823664864
hishamkhattab <khattab648@gmail.com>
2022-06-19 02:00:01 +0200

    create book detail page

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,27 +1,50 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+
+//rducer
+import { updateBook } from "../../store/bookSlice";
+
 //style
 import "./style.css";
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
+    const { allBooks } = useSelector(state => state.books);
+    const dispatch = useDispatch();
+
+    const [bookState, setBookState] = useState(book.shelf);
+
+    const handleSelect = (book, shelf) => {
+        dispatch(updateBook({ book, shelf }))
+        setBookState(shelf);
+    };
+
+    useEffect(() => {
+        const bookShelf = allBooks.find(item => item.id === book.id);
+        if (bookShelf) {
+            setBookState(bookShelf.shelf)
+        } else {
+            setBookState("none")
+        }
+    }, [allBooks, book.id]);
     return (
         <div className="book">
             <div className="book-top">
                 <div
                     className="book-cover"
                     style={{
-                        width: 128,
-                        height: 193,
+                        width: `100%`,
+                        height: `100%`,
                         backgroundImage:
-                        `url(${book.imageLinks.smallThumbnail})`,
+                            `url(${book.imageLinks.smallThumbnail})`,
                     }}
                 ></div>
                 <div className="book-shelf-changer">
-                    <select>
-                        <option value="none" disabled>
+                    <select value={bookState} onChange={({ target }) => handleSelect(book, target.value)}>
+                        <option value="" disabled>
                             Move to...
                         </option>
-                        <option value="currentlyReading">
-                            Currently Reading
-                        </option>
+                        <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
                         <option value="read">Read</option>
                         <option value="none">None</option>
@@ -32,6 +55,13 @@ const Book = ({book}) => {
             <div className="book-authors">{
                 book?.authors?.map((author, idx) => <p key={idx}>{author}</p>)
             }</div>
+            <div className="book-details-btn">
+                <button>
+                    <Link to={`/book/${book.id}`}>
+                        More details
+                    </Link>
+                </button>
+            </div>
         </div>
     );
 }
